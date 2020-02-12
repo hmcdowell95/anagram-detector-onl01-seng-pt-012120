b4fa3afee6dbe88886965c23037939911e62835d
Harley McDowell <hmcdowell95@gmail.com>
2020-02-12 20:54:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end  
+
+  
+
+  def self.match(array)
+    a = self.chars
+    c = []
+    array.each do |x|
+      b = x.chars
+      if a.sort == b.sort
+        c << x
+      end
+    end
+    return c 
+  end
+end
